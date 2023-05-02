41b391838649f65cc6862913df3a6b4669bd62cd
Mateus Tápias <mateustapias2015@gmail.com>
2023-05-02 11:08:22 -0300

    Exercícios pós aula

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript--primeiros-passos/exercicios.js
@@ -0,0 +1,78 @@
+// 1-
+// let a = 3;
+// let b = 2;
+// console.log(
+//   a+b, a-b, a*b, a/b, a%b
+// )
+
+// 2-
+// if (a >= b) console.log(a);
+// else console.log(b);
+
+// 3-
+// let c = 4;
+// if (a > b && a > c) console.log(a);
+// else if (b > a && b > c) console.log(b);
+// else if (c > a && c > b) console.log(c);
+
+// 4-
+// let d = 5;
+// if (d > 0) console.log("positive");
+// else if (d < 0) console.log("negative");
+// else console.log("zero");
+
+// 5-
+// let a1 = 30;
+// let a2 = 60;
+// let a3 = 90;
+
+// if (a1 < 0 || a2 < 0 || a3 < 0) console.log("Valores inválidos");
+// else {
+//   if (a1 + a2 + a3 === 180) console.log(true);
+//   else console.log(false);
+// }
+
+// 6-*
+// let peca = '';
+
+// switch (peca) {
+//   case 'peão':
+//     break;
+// );
+
+// 7-
+// let nota = 20;
+// let total = 25;
+// let porcentagem = nota*100/total
+// console.log(porcentagem + '%');
+// if (porcentagem >= 90) console.log('A');
+// else if (porcentagem >= 80) console.log('B');
+// else if (porcentagem >= 70) console.log('C');
+// else if (porcentagem >= 60) console.log('D');
+// else if (porcentagem >= 50) console.log('E');
+// else console.log('F');
+
+// 8-
+// let a8 = 9;
+// let b8 = 4;
+// let c8 = 5;
+
+// if (a8 % 2 == 0 || b8 % 2 == 0 || c8 % 2 == 0) console.log(true);
+// else console.log(false);
+
+// 9-
+// let a9 = 2;
+// let b9 = 4;
+// let c9 = 8;
+// if (a9 % 2 != 0 || b9 % 2 != 0 || c9 % 2 != 0) console.log(true);
+// else console.log(false);
+
+// 10-
+// let custo = 2000;
+// let imposto = custo*0.2;
+// let venda = 3500;
+// let lucro = venda - (custo + imposto);
+// console.log(lucro);
+
+// 11-
+// MUITO TRAMPO POUCO PROGRESSO
