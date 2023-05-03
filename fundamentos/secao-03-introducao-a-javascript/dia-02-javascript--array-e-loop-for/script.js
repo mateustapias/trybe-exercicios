12fb7fbb124f1b27d3a8479bcd84a1cf88f732fd
Mateus Tápias <mateustapias2015@gmail.com>
2023-05-03 17:20:39 -0300

    Exercicios pré e pós aula

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript--array-e-loop-for/script.js
@@ -0,0 +1,50 @@
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum = 0;
+// let biggestNum = 0;
+
+// for (let v of numbers) {
+//   // console.log(v);
+//   sum += v;
+//   (v > biggestNum) ? biggestNum = v : null;
+// }
+
+// let mean = sum / numbers.length;
+// let smallestNum = biggestNum;
+
+// for (let v of numbers) {
+//   (v < smallestNum) ? smallestNum = v : null;
+// }
+
+// let messages = ['soma: ' + sum,'média: '+ mean, 'maior valor: ' + biggestNum,'menor valor: ' + smallestNum]
+// for (let message of messages) 
+//   console.log(message);
+
+// if (mean > 20) console.log('O valor da média aritmética é maior que 20');
+//   else console.log('O valor da média aritmética é menor ou igual a 20');
+
+// let emptyArray = [];
+// for (let i = 1; i <= 25; i += 1) {
+//   emptyArray.push(i);
+//   console.log(i/2);
+// }
+// console.log(emptyArray);
+
+// #######
+// let max = 10;
+// let facMax = 1;
+// for (let i = max; i > 0; i -= 1)
+//   facMax *= i;
+// console.log(facMax);
+
+// #######
+// let word = 'tryber';
+// let reverseWord = '';
+// // for (let v of word){
+// //   console.log(v)
+// // }
+// for (let i = 0; i < word.length; i+=1) {
+//   reverseWord += word[word.length - 1 -i];
+// }
+// console.log(reverseWord);
+
+// #######
